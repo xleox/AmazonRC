39dd13c953b542635aa7f21c9467bd1fbb9c1cce
xleox <xleox@vip.qq.com>
2018-09-20 17:20:05 +0800

    init

--- api/router.js
+++ api/router.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const chrome = require('./seleTest2');
+
+const severInf = [];
 
 router.get('/',(req,res)=>{
     res.send('Amazon Control Sever Start');
+});
+
+chrome.getHomeInf("xleox@vip.qq.com","asdf1234X");
+
+router.get('/test',(req,res) => {
+    //chrome.getHomeInf("xleox@vip.qq.com","asdf1234X");
+    res.send("testing");
 });
 
 module.exports = router;
--- api/seleTest2.js
+++ api/seleTest2.js
@@ -1,19 +1,33 @@
 var webdriver = require('selenium-webdriver'),
     By = webdriver.By,
     until = webdriver.until;
-
 var chrome = require('selenium-webdriver/chrome');
-var o = new chrome.Options();
-o.addArguments("user-data-dir=C:\\Chrome\\User Data\\");
+
+exports.amazonLogin = function (username,password) {
+    var o = new chrome.Options();
+    o.addArguments("user-data-dir=C:\\Chrome\\User Data\\");
 //o.addArguments("user-data-dir=C:\\Users\\xleox\\AppData\\Local\\Google\\Chrome\\User Data\\");
-var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).setChromeOptions(o).build();
+    var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).setChromeOptions(o).build();
 
-driver.get('http://sellercentral.amazon.com/gp/homepage.html');
+    driver.get('http://sellercentral.amazon.com/gp/homepage.html');
 
-driver.wait(function() {
-    return driver.getTitle()
-        .then( title => {
-            console.log("title: " + title);
-            return title !== "";
-        });
-}, 5000);
+    driver.wait(()=> {
+        return driver.getTitle()
+            .then( title => {  //等待进入界面
+                if(title.indexOf("Amazon") >= 0) return title;
+                else return false;
+            } );}, 5000)
+            .then( title =>{
+                if(title.indexOf("登录") >= 0 || title.indexOf("login") >= 0 ){
+                    console.log("进入账户界面");
+                    console.log(until.elementLocated(By.xpath('//!*[@id="ap_email"]')));
+                    console.log(until.elementLocated(By.xpath('//!*[@id="ap_password"]')));
+                    console.log(until.elementLocated(By.xpath('//!*[@id="authportal-main-section"]/div[2]/div/div/form/div/div/div/div[3]/div/div/label/div/label/input')));
+
+                    /*driver.wait(until.elementLocated(By.xpath('//!*[@id="ap_email"]')), 5000).then(()=>{console.log("用户名框")});
+                    driver.wait(until.elementLocated(By.xpath('//!*[@id="ap_password"]')), 5000).then(()=>{console.log("密码框")});
+                    driver.wait(until.elementLocated(By.xpath('//!*[@id="authportal-main-section"]/div[2]/div/div/form/div/div/div/div[3]/div/div/label/div/label/input')), 5000).then(()=>{console.log("记住框")});
+*/
+                }
+            });
+}
