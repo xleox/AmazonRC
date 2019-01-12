e10678404996707998055105868f81cddf398597
xleox <xleox@vip.qq.com>
2019-01-12 19:50:04 +0800

    修改读取基本信息时间为6分钟

--- api/router.js
+++ api/router.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const Promise = require("bluebird");
 const config = require('./setting').config;
 const 版本={
-    代号:'2.0.7.1',
+    代号:'2.0.7.2',
     名称:'牛刀'
 }
 const sleep = require('sleep');
